11ebd49fd161779986cf7e740b588a00375ea903
D0won <ldwlee03@gmail.com>
2024-10-31 11:39:02 +0900

    ldw/아두이노 함수 따로 둠

--- backend/main.py
+++ backend/main.py
@@ -29,14 +29,7 @@ def capture_and_predict(frame):
     global prediction_result
     # 프레임을 PIL 이미지로 변환
     image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert('L')
-    arduino = serial.Serial('/dev/tty.usbserial-1130', 9600)
-    if arduino.is_open:
-        print("시리얼 포트가 열려있습니다.")
-    else:
-        print("시리얼 포트를 열 수 없습니다.")
     
-    time.sleep(2)
-
     # 이미지를 모델 입력 형식에 맞게 전처리
     preprocess = m.transforms.Compose([
         m.transforms.Grayscale(num_output_channels=1),
@@ -51,13 +44,23 @@ def capture_and_predict(frame):
         prediction = torch.argmax(output, dim=1).item()
     
     prediction_result = {"prediction": prediction}
-    arduino_num = str(prediction) + '\n'
     # 예측된 숫자를 아두이노로 전송
-    arduino.write(arduino_num.encode())
-    arduino.close()
+    sendArduino(prediction)
     
-    
+def sendArduino(prediction) :
+    arduino = serial.Serial('/dev/tty.usbserial-120', 9600)
 
+    if arduino.is_open :
+        print("시리얼 포트가 열려있습니다.")
+
+    else :
+        print("시리얼 포트를 열 수 없습니다.")
+    time.sleep(2)
+
+    arduino_num = str(prediction) + '\n'
+    arduino.write(arduino_num.encode())
+    arduino.close() 
+    
 # 예측 결과를 반환하는 엔드포인트
 @app.get("/prediction")
 async def get_prediction():
