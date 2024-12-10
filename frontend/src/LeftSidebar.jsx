e461fedf0f66461f9ee287d087f28ae0de175f4e
imhyeonuk <gusdnr0316@gmail.com>
2024-12-10 09:10:21 +0900

    LeftSidebar css 조정

--- frontend/src/LeftSidebar.jsx
+++ frontend/src/LeftSidebar.jsx
@@ -21,10 +21,22 @@ function LeftSidebar({
 }) {
   const [showPlayModeOptions, setShowPlayModeOptions] = useState(false);
   const [showVolumeOptions, setShowVolumeOptions] = useState(false);
+
   const handleVolumeChange = (event) => {
     setVolumeLevel(parseFloat(event.target.value));
   };
+
   if (!showLeftSidebar) return null;
+
+  // 메인 아이콘 결정
+  const renderMainPlayModeIcon = () => {
+    if (playMode === 'song') {
+      return <FaMusic />;
+    } else if (playMode === 'alarm') {
+      return <FaBell />;
+    }
+    return <FaMusic />; // 기본 아이콘
+  };
 
   return (
     <div className="leftSidebar">
@@ -41,19 +53,25 @@ function LeftSidebar({
           className="iconButton"
           onClick={() => setShowPlayModeOptions(!showPlayModeOptions)}
         >
-          <FaMusic />
+          {renderMainPlayModeIcon()}
         </button>
         {showPlayModeOptions && (
           <div className="options">
             <button
               className="iconButton"
-              onClick={() => setPlayMode('song')}
+              onClick={() => {
+                setPlayMode('song');
+                setShowPlayModeOptions(false); // 옵션 메뉴 닫기
+              }}
             >
               <FaMusic color={playMode === 'song' ? 'blue' : 'black'} />
             </button>
             <button
               className="iconButton"
-              onClick={() => setPlayMode('alarm')}
+              onClick={() => {
+                setPlayMode('alarm');
+                setShowPlayModeOptions(false); // 옵션 메뉴 닫기
+              }}
             >
               <FaBell color={playMode === 'alarm' ? 'blue' : 'black'} />
             </button>
@@ -86,4 +104,3 @@ function LeftSidebar({
 }
 
 export default LeftSidebar;
-
